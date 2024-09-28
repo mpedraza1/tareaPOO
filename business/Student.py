07a17b250358a79bb4c48a8360f9e5dff1ccaa03
mpedraza1 <90863769+mpedraza1@users.noreply.github.com>
2024-09-28 16:39:38 -0300

    Instucciones

--- business/Student.py
+++ business/Student.py
@@ -12,13 +12,13 @@ class Student():
     def add_user(self, nombre, edad,rut,mail,asignatura,promedio):
         if edad < 0:
             raise ValueError("La edad no puede ser negativa.")
-        self.database.create_record("users", (nombre, edad,rut,mail,asignatura,promedio))
+        self.database.create_record((nombre, edad,rut,mail,asignatura,promedio))
 
     def get_users(self):
         return self.database.read_records()
 
-    def update_user(self, user_id, name, age):
-        self.database.update_record("users", (name, age), user_id)
+    def update_user(self, nombre, edad,rut,mail,asignatura,promedio):
+        self.database.update_record((nombre, edad,mail,asignatura,promedio), rut)
 
-    def delete_user(self, user_id):
-        self.database.delete_record("users", user_id)
+    def delete_user(self, rut):
+        self.database.delete_record(rut)
